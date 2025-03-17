cd9fa1f531d37be7ae4e02d71a1603a5b6d81875
fabiomfaria <fabiomfaria@gmail.com>
2025-03-17 03:06:06 -0300

    1. Domain Events

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleModifiedEvent.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using System;
+
+namespace Ambev.DeveloperEvaluation.Domain.Events
+{
+    public class SaleModifiedEvent
+    {
+        public Guid SaleId { get; }
+        public string SaleNumber { get; }
+        public decimal TotalAmount { get; }
+
+        public SaleModifiedEvent(Sale sale)
+        {
+            SaleId = sale.Id;
+            SaleNumber = sale.SaleNumber;
+            TotalAmount = sale.TotalAmount;
+        }
+    }
+}
